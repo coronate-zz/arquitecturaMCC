db811a04e39af08e447b540630f51092e1114ef4
ALEJANDRO CORONADO NARVAEZ <acoron11@itam.mx>
2017-03-27 16:13:44 -0600

    Se agrega ejercicio 2 NO PROBADO

--- Practica 3/prueba2.s
+++ Practica 3/prueba2.s
@@ -1,34 +1,33 @@
-; Arquitectura de computadoras
-; Programa de demostración. 
+
 .data
 i:	.word32 0
 j:	.word32 0
-uno: .word32 1
 A:  .space 12
+
 .text
-	
+	daddi r3, R0, 2;  Multiplicador
+	daddi r2, R0, i;  i
+	daddi r5, R0, 10; J
 
-	add.d f0,f2, uno(r0);
+	daddi r15, R0, 1; Guardar potencia anterior
+	daddi r10, r0, 0 ;Pointer sobre A
 
-
-	daddi r3, R0, 0;
 	daddi r5,R0, 10;
-	daddi r15, R0, 0;
-
-
 
 
 WHIL:	slt R6, R2, R5
 	beqz R6, ENDW
-	
-	mul.d f2, f4, f6
-	sw R3, j(r0)
+
+	dmul r11, r15, r3; Guarada en A(r10) R15*2
+	sW r11, A(r10);
+	daddi r10, r10, 4;  Mover el apuntador 
+
 
 	daddi r2,r2,1
 	sw	r2,i(r0)
 
-	sW r3, A(r15);
-	daddi r15, r15, 4;
+
+
 
 	j WHIL
 ENDW:	nop
