c1405deccdef7426dd428df9e881a0271a2480b7
coronate <acoronadn@gmail.com>
2017-03-27 15:32:22 -0600

    Se agerga parte del ejercicio 2

--- Practica 3/prueba2.s
+++ Practica 3/prueba2.s
@@ -1,12 +1,41 @@
-      .data
-A:    .word 10
-B:    .word 8
-C:    .word 0
+; Arquitectura de computadoras
+; Programa de demostración. 
+.data
+i:	.word32 0
+j:	.word32 0
+uno: .word32 1
+A:  .space 12
+.text
+	
 
-      .text
-main:
-      ld r4,A(r0)
-      ld r5,B(r0)
-      dadd r3,r4,r5
-      sd r3,C(r0)
-      halt          
+	add.d f0,f2, uno(r0);
+
+
+	daddi r3, R0, 0;
+	daddi r5,R0, 10;
+	daddi r15, R0, 0;
+
+
+
+
+WHIL:	slt R6, R2, R5
+	beqz R6, ENDW
+	
+	mul.d f2, f4, f6
+	sw R3, j(r0)
+
+	daddi r2,r2,1
+	sw	r2,i(r0)
+
+	sW r3, A(r15);
+	daddi r15, r15, 4;
+
+	j WHIL
+ENDW:	nop
+
+
+
+
+
+
+
